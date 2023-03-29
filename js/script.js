7515344ccec6a0524735f8993916eee0e79cce7f
Sarah Reimann <reimannsarah45@gmail.com>
2023-03-29 15:30:34 -0700

    pass test 2
    co-authored-by: Emma Gerigscott <elgerig@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
 function pigLatin(word) {
     const vowels = ["a", "e", "i", "o", "u"];
+    const wordArray = word.split("");
     let pLatin = "";
-    vowels.forEach(function(vowel){
-        if (vowel === word) {
-            pLatin = word + "way";
+    for (let i = 0; i < vowels.length; i++) {
+        if (vowels[i] === wordArray[0]) {
+            return pLatin = (wordArray.join("") + "way");
+        } else if (vowels[i] !== wordArray[0]){
+            return pLatin = wordArray[0];
         }
-    })
+    }
+    // vowels.forEach(function(vowel) {
+    //     if (vowel === wordArray[0]) {
+    //        pLatin.concat(wordArray.join() + "way");
+    //        return pLatin;
+    //     } else if (vowel !== wordArray[0]) {
+    //        pLatin.concat(wordArray.shift());
+    //        return pLatin;
+    //     }
+    // })
+    console.log("word array" + wordArray)
+    console.log("platin " + pLatin)
     return pLatin;
 }
